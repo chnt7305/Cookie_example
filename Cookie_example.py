9fd4a01e49dd9bdb96f10fdcf7716bb7b3844e84
yun <chnt7305@gmail.com>
2015-10-08 19:06:15 +0800

    update

--- Cookie_example.py
+++ Cookie_example.py
@@ -23,7 +23,7 @@ cookie_handler = urllib2.HTTPCookieProcessor(cookie_jar)
 
 #添加HTTP头部，模拟浏览器
 headers = {
-			"GET":"http://www.zhihu.com",
+			"GET":yuanshi_url,
 			"Host":"www.zhihu.com",
 			"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"
 			}
