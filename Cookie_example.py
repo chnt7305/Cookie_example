2de020ed2165cea5f75ec5566ba7dd14b2d29ea1
yun <chnt7305@gmail.com>
2015-10-09 10:52:07 +0800

    update

--- Cookie_example.py
+++ Cookie_example.py
@@ -25,7 +25,7 @@ cookie_handler = urllib2.HTTPCookieProcessor(cookie_jar)
 headers = {
 			"GET":yuanshi_url,
 			"Host":"www.zhihu.com",
-			"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"
+			"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36"
 			}
 request = urllib2.Request(yuanshi_url)
 for key in headers:
